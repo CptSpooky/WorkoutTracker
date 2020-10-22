29237c26621b0f6b4109286ddfa46da746b82aa0
CptSpooky <legacyofthedonut@gmail.com>
2020-10-22 11:47:31 -0400

    code cleanup

--- routes/view.js
+++ routes/view.js
@@ -2,12 +2,17 @@
 const router = require("express").Router();
 const path = require("path");
 
+// Get index page
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+// Get exercise page
 router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
+
+// Get stats page
 router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
